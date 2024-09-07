baee4c1784e9e239da4d4a94e9246dfa39550547
Vio <veo@pop.os>
2024-09-07 21:51:03 +0200

    chore: introduce basename param for BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <BrowserRouter>  {/* Wrap the App in BrowserRouter */}
+      <BrowserRouter basename='/BadgeSVGen'>  {/* Wrap the App in BrowserRouter */}
         <App />
       </BrowserRouter>
     </React.StrictMode>
--- src/utils/generateShareableLink.ts
+++ src/utils/generateShareableLink.ts
@@ -3,5 +3,5 @@ import { encodeToBase64 } from "./base64Utils";
 
 export const generateShareableLink = (inputs: any) => {
   const base64String = encodeToBase64(inputs);
-  return `${window.location.origin}?data=${base64String}`;
+  return `${window.location.origin}${window.location.pathname}?data=${base64String}`;
 };
